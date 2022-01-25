3917de5743aa85c33521f96ca459aa0161ee50e0
Jack <loaizajack@gmail.com>
2022-01-25 20:41:21 +0000

    base hero styling

--- components/SearchHero.js
+++ components/SearchHero.js
@@ -1,5 +1,9 @@
 import classes from '../styles/components/SearchHero.module.scss'
 import {useEffect, useRef, useState} from "react";
+import {faBriefcaseMedical, faPlateUtensils, faPanFood} from "@fortawesome/pro-thin-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
 
 export default function SiteHero() {
 
@@ -9,49 +13,77 @@ export default function SiteHero() {
     const bgContainerRef = useRef();
     let imageSelectInterval = null;
 
-    useEffect(() => {
-            setImageCountState()
-            setInterval(selectRandomImage, 3000)
-        }, []
-    );
 
 
-    const setImageCountState = function () {
-        setImageCount(calculateImageCount())
+useEffect(() => {
+        setImageCountState()
+        selectRandomImage()
+        setInterval(selectRandomImage, 3000)
+    }, []
+);
+
+const setImageCountState = function () {
+    setImageCount(calculateImageCount())
+}
+
+const calculateImageCount = function () {
+    let targetHeight = bgContainerRef.current.clientHeight;
+    let slidesPerRow = Math.ceil(bgContainerRef.current.clientWidth / imageRef.current.clientWidth);
+    let imageHeight = imageRef.current.clientHeight
+    let rows = 0;
+    while ((rows * imageHeight) < targetHeight) {
+        rows += 1
     }
+    return rows * slidesPerRow;
 
-    const calculateImageCount = function () {
-        let targetHeight = bgContainerRef.current.clientHeight;
-        let slidesPerRow = Math.ceil(bgContainerRef.current.clientWidth / imageRef.current.clientWidth);
-        let imageHeight = imageRef.current.clientHeight
-        let rows = 0;
-        while ((rows * imageHeight) < targetHeight) {
-            rows += 1
-        }
+}
 
-        return rows * slidesPerRow;
+const selectRandomImage = function () {
+    let min = 0;
+    let max = calculateImageCount();
 
+    setActiveImage(Math.floor(Math.random() * (max - min + 1)) + min)
+}
 
-    }
+const getRandomMealImage = function (int) {
+    const images = ['foodboard.png', 'pasta.png', 'steak.png', 'eggs.png', 'veg.png', 'toast.png', 'asdfihvasd.png', 'vnabisudfb.png', 'vansldifvasdf.png', 'asdvaoisf.png', 'aiywegufsadf.png']
+    return images[int % images.length]
+}
 
-    const selectRandomImage = function () {
-        let min = 0;
-        let max = calculateImageCount();
+return (
+    <div className={classes.searchHero}>
+        <div className={classes.searchHeroBgContainer} ref={bgContainerRef}>
+            {[...Array(imageCount)].map((ele, int) => (
+                (<div
+                    key={int}
+                    className={`${int === activeImage ? classes.searchHeroImageActive : classes.searchHeroImage}`}
+                    ref={imageRef}
+                    style={{backgroundImage: `url('/img/meals/${getRandomMealImage(int)}')`}}
+                />)
+            ))}
+        </div>
+        <div className={classes.content}>
+            <h1 className={classes.title}>Find Food Here</h1>
+            <h3 className={classes.subtitle}>
+                Personalise Your search
+            </h3>
+            <div className={classes.selector}>
+                <button className={classes.selectorButton}>
+                    <FontAwesomeIcon icon={faBriefcaseMedical}/>
+                    <span>Allergy <br/>Information</span>
+                </button>
 
-        setActiveImage(Math.floor(Math.random() * (max - min + 1)) + min)
-    }
+                <button className={classes.selectorButton}>
+                    <FontAwesomeIcon icon={faPlateUtensils}/>
+                    <span>Diet <br/>Information</span>
+                </button>
 
-    return (
-        <div className={classes.searchHero}>
-            <div className={classes.searchHeroBgContainer} ref={bgContainerRef}>
-                {[...Array(imageCount)].map((ele, int) => (
-                    (<div
-                        key={int}
-                        className={`${int === activeImage ? classes.searchHeroImageActive :classes.searchHeroImage}`}
-                        ref={imageRef}
-                    />)
-                ))}
+                <button className={classes.selectorButton}>
+                    <FontAwesomeIcon icon={faPanFood}/>
+                    <span>Favourite<br/>Dishes</span>
+                </button>
             </div>
         </div>
-    )
+    </div>
+)
 }
--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -1,5 +1,18 @@
+import classes from '../styles/components/SiteHeader.module.scss'
+import Link from 'next/link'
+
 export default function SiteHeader() {
     return (
-        <></>
+        <header className={classes.header}>
+            <Link href={'/'}>
+                <a>
+                    <img src="/img/logo/logo.svg" alt=""/>
+                </a>
+            </Link>
+
+            <button className={classes.accountButton}>
+                <img src="/icons/user-thin.svg" alt="Profile"/>
+            </button>
+        </header>
     )
 }
